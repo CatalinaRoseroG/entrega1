71540d4635f810a6eb88ff1147a3eb3dab7d862f
CatalinaRoseroG <145790624+CatalinaRoseroG@users.noreply.github.com>
2023-10-30 20:36:04 -0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart'; // Importa la biblioteca dio
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SignUpScreen(),
+      home: MyHomePage(),//MyHomePage
     );
   }
 }
 
-class SignUpScreen extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
+
+   final TextEditingController nombreController = TextEditingController();
+   final TextEditingController correoController = TextEditingController();
+   final TextEditingController contraController = TextEditingController();
+
+   Future<void> cargarDatosDelUsuario() async{
+  final url = Uri.parse('http://192.168.56.1/webService/ejemplopost.php');
+
+  final response = await http.post(
+    url,
+    body: {
+      'nombre': nombreController.text,
+      'correo': correoController.text,
+      'contraseña': contraController.text,
+    }
+  );
+
+   if (response.statusCode == 200) {
+      // La solicitud se realizó con éxito
+      final responseData = response.body;
+      print(responseData); // Puedes procesar la respuesta aquí
+    } else {
+      // Hubo un error en la solicitud
+      print('Error: ${response.statusCode}');
+      print('Mensaje de error: ${response.body}');
+    }
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,12 +55,15 @@ class SignUpScreen extends StatelessWidget {
         child: Column(
           children: [
             TextField(
+              controller: nombreController,
               decoration: InputDecoration(labelText: 'Nombre de usuario'),
             ),
             TextField(
+              controller: correoController,
               decoration: InputDecoration(labelText: 'Correo electrónico'),
             ),
             TextField(
+              controller: contraController,
               decoration: InputDecoration(labelText: 'Contraseña'),
               obscureText: true,
             ),
@@ -45,19 +77,5 @@ class SignUpScreen extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-void cargarDatosDelUsuario() async{
-  try{
-  final response = await dio.get('http://172.16.8.218/flutter_pruebafinal/usuarios.php');
-
- if (response.statusCode == 200){
-   print(response.data); // Puedes imprimir la respuesta o realizar otras acciones.
-    } else {
-      print('Error en la solicitud: ${response.statusCode}');
-    }
-  } catch (e) {
-    print('Error: $e');
   }
 }
